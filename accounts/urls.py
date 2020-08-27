0d5332ea2789f63fa15f5f7c32d1c5920271cf55
zacharatos <rkeytech@protonmail.com>
2020-08-27 19:07:49 +0300

    Dynamic Content

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard),
-    path('products/', views.products),
-    path('customers/', views.customers),
+    path('', views.dashboard, name='dashboard'),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('create_order/', views.createOrder, name='create_order')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,47 @@
 from django.shortcuts import render
+from .models import Customer, Product, Order
 
 # Create your views here.
 
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total = {
+        'total': orders.count(),
+        'delivered': orders.filter(status='Delivered').count(),
+        'pending': orders.filter(status='Pending').count()
+    }
+
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total': total
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products': products})
 
 
-def customers(request):
-    return render(request, 'accounts/customers.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    total_orders = orders.count()
+
+    context = {
+        'customer': customer,
+        'orders': orders,
+        'total_orders': total_orders
+    }
+    return render(request, 'accounts/customer.html', context)
+
+
+def createOrder(request):
+
+    context = {}
+    return render(request, 'accounts/order_form.html', context)
